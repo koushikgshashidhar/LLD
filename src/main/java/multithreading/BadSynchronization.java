1e6d914fa3674b31f6558be251dceb1b2a0c0d49
koushikgshashidhar <koushikshashidhar@gmail.com>
2022-12-18 23:18:10 +0530

    Merge branch 'main' of https://github.com/koushikgshashidhar/LLD

---
+++ src/main/java/multithreading/BadSynchronization.java
@@ -0,0 +1,23 @@
+package multithreading;
+
+class BadSynchronization {
+    public static void main(String args[]) throws InterruptedException {
+        Object dummyObject = new Object();
+// Attempting to call wait() on the object
+// outside of a synchronized block.
+        dummyObject.wait();
+    }
+}
+
+class BadSynchronization1 {
+    public static void main(String args[]) {
+        Object dummyObject = new Object();
+        Object lock = new Object();
+        synchronized (lock) {
+            lock.notify();
+// Attempting to call notify() on the object
+// in synchronized block of another object
+            dummyObject.notify();
+        }
+    }
+}
---
+++ src/main/java/multithreading/Employee.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+class Employee {
+    // shared variable
+    private String name;
+
+    private Object lock = new Object();
+    // method is synchronize on 'this' object
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
+    // also synchronized on the same object
+    public synchronized void resetName() {
+        this.name = "";
+    }
+    // equivalent of adding synchronized in method
+// definition
+    public String getName() {
+// Using a different object to synchronize on
+        synchronized (lock) {
+            return this.name;
+        }
+    }
+}
